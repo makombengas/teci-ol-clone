278fee822774db60fb1d33b51c3ac070e92934f7
makombengas <berlioz2001@yahoo.fr>
2024-10-28 16:07:28 +0100

    change agains on next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,8 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      os: false,
-    };
-    return config;
+    config.resolve.fallback = { fs: false }
+    return config
   },
   pageExtensions: ['jsx', 'js'],
 };
