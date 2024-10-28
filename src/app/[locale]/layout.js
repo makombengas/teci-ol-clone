a7dfe2674eff6f07560d0da08e0e27b6bae5d7de
makombengas <berlioz2001@yahoo.fr>
2024-10-28 15:40:59 +0100

    change agains on locale/layout.js

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,4 +1,3 @@
-
 import { notFound } from 'next/navigation';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
 import { ReactNode } from 'react';
@@ -6,36 +5,43 @@ import BaseLayout from '@/components/BaseLayout';
 import { routing } from '@/i18n/routing';
 import '../globals.css';
 
+
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
+  return routing.locales.map((locale) => ({ locale }));
 }
+
 
 export async function generateMetadata({ params }) {
-  const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: 'LocaleLayout' });
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'LocaleLayout' });
 
-  return {
-    title: t('title')
-  };
+
+  return {
+    title: t('title')
+  };
 }
 
+
 export default async function LocaleLayout({ children, params }) {
-  const { locale } = await params;
+  const { locale } = await params;
 
-  // Ensure that the incoming `locale` is valid
-  if (!routing.locales.includes(locale)) {
-    notFound();
-  }
 
-  // Enable static rendering
-  setRequestLocale(locale);
+  // Ensure that the incoming `locale` is valid
+  if (!routing.locales.includes(locale)) {
+    notFound();
+  }
 
-  return (
-    <BaseLayout locale={locale}>
-      <div>
-        <title>{generateMetadata({ params }).title}</title>
-      </div>
-      {children}
-    </BaseLayout>
-  );
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+
+  return (
+    <BaseLayout locale={locale}>
+      <div>
+        <title>{generateMetadata({ params }).title}</title>
+      </div>
+      {children}
+    </BaseLayout>
+  );
 }
